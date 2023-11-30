802211199b9da3b5654a4c35486ef392f808a19e
Arindal Char <110285827+arindal1@users.noreply.github.com>
2023-11-30 22:14:20 +0530

    Create net_spd.py

---
+++ network_speedcheck/net_spd.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import speedtest
+import time
+from speedtest import ConfigRetrievalError
+
+def get_speed_test_results(log_file="speed_test_log.txt"):
+    try:
+        st.text("Running speed test...")
+        
+
+        stt = speedtest.Speedtest()
+        stt.get_best_server()
+
+        download_speed = stt.download() / 1_000_000  # Convert to Mbps
+        upload_speed = stt.upload() / 1_000_000  # Convert to Mbps
+
+        st.text(f"Download Speed: {download_speed:.2f} Mbps")
+        st.text(f"Upload Speed: {upload_speed:.2f} Mbps")
+
+        # Log results to a file
+        log_entry = f"{time.strftime('%Y-%m-%d %H:%M:%S')} - Download Speed: {download_speed:.2f} Mbps, Upload Speed: {upload_speed:.2f} Mbps\n"
+        with open(log_file, "a") as log_file:
+            log_file.write(log_entry)
+
+        return download_speed, upload_speed
+    except ConfigRetrievalError as e:
+        st.text(f"Error: {e}. Check your network connection.")
+        # Log errors to a file
+        error_entry = f"{time.strftime('%Y-%m-%d %H:%M:%S')} - Error: {str(e)}\n"
+        with open(log_file, "a") as log_file:
+            log_file.write(error_entry)
+        return None, None
+
+if __name__ == "__main__":
+    st.title("Network Speed Tester")
+    st.warning("This may take a moment.")
+
+    while True:
+        download_speed, upload_speed = get_speed_test_results()
+
+        # Update every second
+        time.sleep(1)
+
+        # Clear previous results
+        st.text("")  # Clear the "Running speed test..." message
+        st.text("")  # Clear the previous results
